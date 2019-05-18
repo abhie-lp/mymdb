11dc522ffcf0321c70dfaea44ada9faf6d90be88
Abhishek Tiwari <abhie.lp@gmail.com>
2019-05-18 12:36:44 +0530

    MovieDetail view

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 app_name = "core"
 
 urlpatterns = [
+    path("movie/<int:pk>/", views.MovieDetailView.as_view(), name="detail"),
     path("movies/", views.MovieListView.as_view(), name="list"),
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,7 @@ from django.views import generic
 
 class MovieListView(generic.ListView):
     model = models.Movie
+
+
+class MovieDetailView(generic.DetailView):
+    model = models.Movie
