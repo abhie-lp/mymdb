8d8a236d9a007d8a445d80a73bacd9979e1e9c41
Abhishek Tiwari <abhie.lp@gmail.com>
2019-05-18 09:52:05 +0530

    Added Movie model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
+from . import models
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(models.Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = "id", "title", "year", "runtime", "get_rating_display",
+    list_display_links = "title",
+    list_filter = "year", "runtime",
+    search_fields = "title", "year",
