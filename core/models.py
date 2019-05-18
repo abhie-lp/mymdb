0fcb33d25d06f921ee727839d9064f142ac48ec1
Abhishek Tiwari <abhie.lp@gmail.com>
2019-05-19 01:16:10 +0530

    Added MovieManager with select_related  method

--- core/models.py
+++ core/models.py
@@ -27,6 +27,18 @@ class Person(models.Model):
         return self.first_name + " " + self.last_name
 
 
+class MovieManager(models.Manager):
+
+    def all_with_related_persons(self):
+        qs = self.get_queryset()
+        qs = qs.select_related(
+            "director"
+        )
+        qs = qs.prefetch_related("writers", "actors")
+
+        return qs
+
+
 class Movie(models.Model):
     NOT_RATED = 0
     RATED_G = 1
@@ -64,6 +76,8 @@ class Movie(models.Model):
         related_name="acting_credits",
         blank=True
     )
+
+    objects = MovieManager()
 
     class Meta:
         ordering = "-year", "title",
